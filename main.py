122665e4507718aa5578109c39ac7a43853a021c
Yeslem <yeslem5223@gmail.com>
2021-07-05 01:28:48 +0100

    initialiser main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, request
+from lib import functions as func
+import lib.words as words
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Assitante vocal page d'accueil<h2/>"
+
+@app.route('/assistant_logiciel/api/translate_text_to_hasaniya', methods=['POST'])
+def reception_envoie():
+    content = request.get_json(force=True)
+    return jsonify({'result': func.hsaniya_google_to_hasniya_general(content['text'], words=words.words)})
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host= "0.0.0.0", port=5640)
